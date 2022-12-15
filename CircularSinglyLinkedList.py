1758a51f1d4989aa2cc7dcf0a0c2cc4dda8d18e3
Priyank Sharma <priyank.sharma@consolebit.com>
2022-12-15 10:46:19 +0530

    inital commit

---
+++ CircularSinglyLinkedList.py
@@ -0,0 +1,183 @@
+# x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x
+# x--x--x--x--x--x--------------------------CIRCULAR SINGLY LINKED LIST---------------------------------x--x--x--x--x--x
+# x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x
+
+
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+
+class CircularSinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            if node.next == self.head:
+                break
+            node = node.next
+
+    # --------------------------Creation of Circular Singly Linked List------------------------------------
+
+    def createCSLL(self, value):
+        node = Node(value)
+        node.next = node
+        self.head = node
+        self.tail = node
+        return "Circular Linked List Download is created"
+
+    # ------------------------------------Insertion in Circular Singly Linked List--------------------------------------
+
+    def insertCSLL(self, value, location):
+        newNode = Node(value)
+        if self.head is None:
+            # return "the head reference is None"
+            self.head = newNode
+            newNode.next = self.head
+            self.tail = newNode
+            return
+
+        # ----another method for insertion at last-----
+        # currentNode = self.head
+        # while currentNode.next is not self.head:
+        #     currentNode = currentNode.next
+        # currentNode.next = newNode
+        # newNode.next = self.head
+
+        else:
+            if location == 0:
+                newNode.next = self.head
+                self.head = newNode
+                self.tail.next = newNode
+
+            elif location == -1:
+                newNode.next = self.tail.next
+                self.tail.next = newNode
+                self.tail = newNode
+
+            else:
+                currentNode = self.head
+                index = 0
+                while index < location - 1:
+                    currentNode = currentNode.next
+                    index += 1
+                nextNode = currentNode.next
+                currentNode.next = newNode
+                newNode.next = nextNode
+                if currentNode == self.tail:
+                    self.tail = newNode
+                    self.tail.next = self.head
+
+    # -------------------------------------traversing in Circular Singly Linked List------------------------------------
+
+    def traverseCSLL(self):
+        if self.head is None:
+            return "the linkedlist doesn't exist"
+
+        else:
+            currentNode = self.head
+            while True:
+                print(currentNode.value)
+                if currentNode.next is self.head:
+                    break
+                currentNode = currentNode.next
+
+    # ------------------------------------Searching in Circular Singly Linked List--------------------------------------
+
+    def searchCSLL(self, item):
+        if self.head is None:
+            return "the linkedlist doesn't exist"
+        else:
+            currentNode = self.head
+            index = 0
+            while True:
+                if currentNode.value == item:
+                    print("the item present at index", index)
+                if currentNode.next is self.head:
+                    print("the item", "(", item, ")", "does not exist")
+                    break
+                currentNode = currentNode.next
+                index += 1
+
+    # ------------------------------------Deletion In Circular Linked List----------------------------------------------
+
+    def deletionCSLL(self, location):
+        if self.head is None:
+            return "the linkedlist doesn't exist"
+
+        else:
+            if location == 0:
+                if self.head == self.tail:
+                    self.head.next = None
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.head = self.head.next
+                    self.tail.next = self.head
+
+            elif location == -1:
+                if self.head == self.tail:
+                    self.head.next = None
+                    self.head = None
+                    self.tail = None
+                else:
+                    currentNode = self.head
+                    while True:
+                        if currentNode.next == self.tail:
+                            break
+                        currentNode = currentNode.next
+                    currentNode.next = self.head
+                    self.tail = currentNode
+
+            else:
+                currentNode = self.head
+                index = 0
+                while index < location - 1:
+                    currentNode = currentNode.next
+                    index += 1
+                nextNode = currentNode.next
+                currentNode.next = nextNode.next
+
+    # ------------------------------------print method------------------------------------------------------------------
+
+    def printList(self):
+        currentNode = self.head
+        while True:
+            print(currentNode.value)
+            if currentNode.next is self.head:
+                break
+            currentNode = currentNode.next
+
+    # ---------------------------------------------delete entire SSL---------------------------------------------------
+
+    def delete_entire(self):
+        if self.head is None:
+            print("the singly linked list does not exist")
+        else:
+            self.head = None
+            self.tail.next = None
+            self.tail = None
+
+
+CSLinkedList = CircularSinglyLinkedList()
+# CSLinkedList.createCSLL('aman')
+CSLinkedList.insertCSLL('aman', 0)
+CSLinkedList.insertCSLL('deep', 1)
+CSLinkedList.insertCSLL('breydan', 2)
+CSLinkedList.insertCSLL('logan', 3)
+CSLinkedList.insertCSLL('tincy', 4)
+# CSLinkedList.delete_entire()
+CSLinkedList.traverseCSLL()
+# CSLinkedList.searchCSLL('erer')
+print([node.value for node in CSLinkedList])
+CSLinkedList.deletionCSLL(2)
+# # print([node.value for node in CSLinkedList])
+# # CSLinkedList.insertCSLL('priyank', 4)
+print([node.value for node in CSLinkedList])
+# print([])
+CSLinkedList.printList()
---
+++ SinglyLinkedList.py
@@ -0,0 +1,158 @@
+# x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x
+# x--x--x--x--x--x-------------------------------SINGLY LINKED LIST-------------------------------------x--x--x--x--x--x
+# x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x--x
+
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        # self.count = 0
+
+    def __iter__(self):
+        node = self.head
+        while node:
+            yield node
+            node = node.next
+
+    # --------------------insertion in SSL---------------------
+
+    def append_item(self, data):
+        # Append items on the list
+        node = Node(data)
+
+        if self.head is None:
+            self.head = node
+            self.tail = node
+
+        else:
+            self.tail.next = node
+            self.tail = node
+        # self.count += 1
+
+    def insertSSL(self, value, location):
+        newNode = Node(value)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+        else:
+            if location == 0:
+                newNode.next = self.head
+                self.head = newNode
+
+            elif location == -1:
+                newNode.next = None
+                self.tail.next = newNode
+                self.tail = newNode
+
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode
+                if tempNode == self.tail:
+                    self.tail = newNode
+
+    # ----------------------------treverse in SSL----------------------------
+
+    def treverse(self):
+        if self.head is None:
+            print("the singly linked list does not exist")
+        else:
+            node = self.head
+            while node is not None:
+                print(node.value)
+                node = node.next
+
+    # -------------------------------------Search in SSL----------------------------------
+
+    def search(self, item):
+        if self.head is None:
+            print("the singly linked list does not exist")
+        else:
+            node = self.head
+            index = 0
+            while node is not None:
+                if node.value == item:
+                    print("item found at index", index)
+                index += 1
+                node = node.next
+
+    # -------------------------------------deletion in SSL--------------------------------
+
+    def deletion(self, location):
+        if self.head is None:
+            print("the singly linked list does not exist")
+        else:
+            if location == 0:
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    self.head = self.head.next
+            elif location == -1:
+                if self.head == self.tail:
+                    self.head = None
+                    self.tail = None
+                else:
+                    node = self.head
+                    while True:
+                        if node.next == self.tail:
+                            break
+                        node = node.next
+                    node.next = None
+                    self.tail = node
+            else:
+                tempNode = self.head
+                index = 0
+                while index < location - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = nextNode.next
+
+    # ---------------------------------------------delete entire SSL---------------------------------------------------
+
+    def delete_entire(self):
+        if self.head is None:
+            print("the singly linked list does not exist")
+        else:
+            self.head = None
+            self.tail = None
+
+    def __getitem__(self, index):
+        # if index > self.count - 1:
+        #     return "Index out of range"
+        current_val = self.head
+        for n in range(index):
+            current_val = current_val.next
+        return current_val.value
+
+
+singlylinkedlist = SLinkedList()
+singlylinkedlist.append_item('aman')
+singlylinkedlist.append_item('deep')
+singlylinkedlist.append_item('breydan')
+singlylinkedlist.append_item('logan')
+singlylinkedlist.append_item('tincy')
+# singlylinkedlist.insertSSL(33, 3)
+# singlylinkedlist.insertSSL(33, 1)
+# singlylinkedlist.insertSSL(33, 3)
+
+# # print("[0,1,2,3,4]")
+print([node.value for node in singlylinkedlist])
+# # singlylinkedlist.treverse()
+# # singlylinkedlist.search(33)
+# singlylinkedlist.deletion(-1)
+# print([node.value for node in singlylinkedlist])
+# singlylinkedlist.delete_entire()
+print(singlylinkedlist[3])
